ab9ec48643bd5b7c187e14f375804cb4fcdeaa25
Oscar Ablinger <dev.ablinger@gmail.com>
2021-03-10 19:48:02 +0100

    feat(dtos): implement DTOs

---
+++ uld-fs-dtos/LanguageDefinition.fs
@@ -0,0 +1,44 @@
+module ULD.Fs.DTOs
+
+type public CommentDefinition = {
+  startMarker: string
+  endMarker: string
+  treatAs: string
+}
+
+type public CommentsDefinition = {
+  documentationComments: CommentDefinition list
+  normalComments: CommentDefinition list
+}
+
+type public SymbolDefinition =
+  | Action of command: string
+  | NonTerminal of referencedRule: string
+  | OneOf of allowNone: bool * options: string list
+  | Whitespace
+  | LineEnd
+  | String of text: string
+  | Digit
+  | Letter
+  | LetterOrDigit
+  | LowercaseLetter
+  | UppercaseLetter
+  | Character
+  | CharacterOf of chars: char list
+  | CharacterExcept of chars: char list
+
+let (|Terminal|) (ruleDef: SymbolDefinition) =
+  match ruleDef with
+  | Action _ -> false
+  | NonTerminal _ -> false
+  | OneOf _ -> false
+  | _ -> true
+
+type public LanguageDefinition = {
+  name: string option
+  filePattern: string option
+  version: string
+  comments: CommentsDefinition
+  startRules: string list
+  rules: Map<string, SymbolDefinition list>
+}
